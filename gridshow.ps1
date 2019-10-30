2d4fe4347a83a45a690379e99519d5eeff40bceb
BoSen29 <54361737+BoSen29@users.noreply.github.com>
2019-10-30 14:39:05 +0100

    added full dashboard
    and stuffs

--- gridshow.ps1
+++ gridshow.ps1
@@ -1,15 +1,28 @@
-New-UDInput -Title "Computer Lookup"  -Endpoint {
-    param($user)
-    $search = "*$user*"
-
-    $session:griddata = (get-adcomputer -properties * -filter { Description -like $search } | Select-Object Name, Description) 
-    Sync-UDElement -id "GridContainer"
-    New-UDInputAction -ClearInput           
-}
-New-UDElement -tag span -id "GridContainer" -Endpoint {
-    if ($null -ne $session:griddata) {
-        New-UDGrid -Title "Results" -id "ResultsGrid" -headers @("Computer Name", "Description") -properties @("Name","Description") -endpoint {
-            $session:griddata |  Out-UDGridData
+$dash = New-UDDashboard -Title "Test" -Content {
+    New-UDInput -Title "Computer Lookup"  -Endpoint {
+        param($user)
+        $search = "*$user*"
+    
+        $session:griddata = [PSCustomObject]@{
+            Name = "BoSen29"
+            Description = "Dude"
+        } | Select-Object Name, Description
+        Sync-UDElement -id "GridContainer"
+        New-UDInputAction -ClearInput           
+    }
+    New-UDElement -tag span -id "GridContainer" -Endpoint {
+        if ($null -ne $session:griddata) {
+            New-UDGrid -Title "Results" -id "ResultsGrid" -headers @("Computer Name", "Description") -properties @("Name","Description") -endpoint {
+                $session:griddata |  Out-UDGridData
+            }
         }
+
+    }    
+    
+    New-UDButton -Text "RemoveMe" -OnClick {
+        $session:griddata = $null
+        Sync-UDElement -id "GridContainer"
     }
 }
+Get-UDDashboard | Stop-UDDashboard
+Start-UDDashboard -Dashboard $dash
